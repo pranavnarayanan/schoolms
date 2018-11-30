5f3d78b616235024df3238d96ffa37ec79fb0f25
Aravind R Pillai <aravindrpillai1992@gmail.com>
2018-11-30 07:44:09 +0530

    database

--- schoolms/settings.py
+++ schoolms/settings.py
@@ -92,6 +92,10 @@ WSGI_APPLICATION = 'schoolms.wsgi.application'
 
 DATABASES = {
     'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'sql_db':{
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'schoolms',
         'USER': 'root',
@@ -103,8 +107,6 @@ DATABASES = {
         }
     }
 }
-#'ENGINE': 'django.db.backends.sqlite3',
-#'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
 
 AUTH_PASSWORD_VALIDATORS = [
