db58150b1855fab55635b53b8a5f2dcea2567fb8
Aravind R Pillai <aravind.ramachandran.pillai@pwc.com>
2018-11-30 12:28:31 +0530

    documents template file

--- apps/documents/views.py
+++ apps/documents/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from apps.utilities.helper.ui_data_helper import UIDataHelper
 
 def index(request):
-    return HttpResponse("List all documnets of User")
+    template = loader.get_template("doc_list_my_documenst.html")
+    data = UIDataHelper(request).getData(page="is_list_my_documents")
+    return HttpResponse(template.render(data, request))
 
 def uploadNewFile(request):
     return HttpResponse("Page to upload new document")
--- schoolms/settings.py
+++ schoolms/settings.py
@@ -95,17 +95,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'sql_db':{
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'schoolms',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-        'OPTIONS': {
-            'sql_mode': 'traditional',
-        }
-    }
 }
 
 
