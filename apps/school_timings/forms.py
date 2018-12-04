44a25e2a3ccaf02ede13fbaaba86c85c38d956ce
Aravind R Pillai <aravindrpillai1992@gmail.com>
2018-12-04 07:57:10 +0530

    school timings

--- apps/school_timings/forms.py
+++ apps/school_timings/forms.py
@@ -1,6 +1,29 @@
+import json
 from django import forms
+from apps.roles.models import EN_UserRoles
 from apps.school_timings.helper.school_timing_helper import SchoolTimingsHelper
+from apps.school_timings.models import EN_SchoolTimings
 from properties.calendar import DaysChoiceList
+from properties.session_properties import SessionProperties
+
+
+class FORM_AddModifySchoolTiming_Page1(forms.Form) :
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(FORM_AddModifySchoolTiming_Page1, self).__init__(*args, **kwargs)
+        active_role = json.loads(self.request.session[SessionProperties.USER_SELECTED_ROLE_KEY])
+        active_role = EN_UserRoles.objects.get(id=active_role["role_id"])
+        schoolTimings = EN_SchoolTimings.objects.filter(organization=active_role.related_organization)
+        choiceList = []
+        for st in schoolTimings:
+            ch = (st.id,"{} - {} to {} ".format(st.timing_name,st.school_starting_time,st.school_closing_time))
+            choiceList.append(ch)
+        self.fields['available_timings'].choices = tuple(choiceList)
+
+    timing_name       = forms.CharField( max_length=50, required=False )
+    available_timings = forms.ChoiceField(required=False, choices=[])
+
 
 class FORM_SchoolTiming(forms.Form) :
 
--- apps/school_timings/views.py
+++ apps/school_timings/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from apps.roles.models import EN_UserRoles
-from apps.school_timings.forms import FORM_SchoolTiming
+from apps.school_timings.forms import FORM_SchoolTiming, FORM_AddModifySchoolTiming_Page1
 from apps.school_timings.models import EN_SchoolTimings
 from apps.utilities.helper.ui_data_helper import UIDataHelper
 from displaykey.display_key import DisplayKey
@@ -18,8 +18,10 @@ def index(request):
 
 def addSchoolTimings(request):
     data = UIDataHelper(request).getData(page="is_add_school_timings")
-    data.__setitem__("form",FORM_SchoolTiming(request.POST) if request.method == "POST" else FORM_SchoolTiming())
-    template = loader.get_template("school_timings_add_new_timing.html")
+    data.__setitem__("form",FORM_AddModifySchoolTiming_Page1(request.POST, request=request) if request.method == "POST" else FORM_AddModifySchoolTiming_Page1(request=request))
+    #template = loader.get_template("school_timings_add_new_timing.html")
+    template = loader.get_template("school_timings_add_modify_timing.html")
+    messages.info(request,"You can create a new timing or modify an existing timing")
     return HttpResponse(template.render(data, request))
 
 
@@ -31,45 +33,28 @@ def submitSchoolDetails(request):
 
             user_role = EN_UserRoles.objects.filter(approved=True,user_id=user_id,is_selected_role=True, role__code=Roles.SCHOOL_ADMIN)
             if user_role.exists():
-
-                timing_name = form_data["timing_name"].value()
-                school_start_time = form_data["school_start_time"].value()
-                school_closing_time = form_data["school_closing_time"].value()
-                total_periods = form_data["total_periods"].value()
-                school_off_days = form_data["school_off_days"].value()
-                assembly_on_days = form_data["assembly_on_days"].value()
-                assembly_duration = form_data["assembly_duration"].value()
-
                 user_role = user_role.first()
                 if(user_role.related_organization != None):
 
-
+                    timing_name = form_data["timing_name"].value()
+                    school_start_time = form_data["school_start_time"].value()
+                    school_closing_time = form_data["school_closing_time"].value()
+                    total_periods = form_data["total_periods"].value()
+                    school_off_days = form_data["school_off_days"].value()
+                    assembly_on_days = form_data["assembly_on_days"].value()
+                    assembly_duration = form_data["assembly_duration"].value()
 
                     try:
                         st = EN_SchoolTimings()
-                        st.organization                      = user_role.related_organization
-                        st.timing_name                       = timing_name
-                        st.school_starting_time              = school_start_time
-                        st.school_closing_time               = school_closing_time
-                        st.breakfast_starting_time           = breakfast_break_starting_time
-                        st.breakfast_ending_time             = breakfast_break_ending_time
-                        st.lunch_starting_time               = lunch_break_starting_time
-                        st.lunch_ending_time                 = lunch_break_ending_time
-                        st.dinner_starting_time              = dinner_break_starting_time
-                        st.dinner_ending_time                = dinner_break_ending_time
-                        st.no_of_periods                     = int(total_periods)
-                        st.first_interval_starting_time      = interval_1_starting_time
-                        st.first_interval_ending_time        = interval_1_ending_time
-                        st.second_interval_starting_time     = interval_2_starting_time
-                        st.second_interval_ending_time       = interval_2_ending_time
-                        st.third_interval_starting_time      = interval_3_starting_time
-                        st.third_interval_ending_time        = interval_3_ending_time
-                        st.fourth_interval_starting_time     = interval_4_starting_time
-                        st.fourth_interval_ending_time       = interval_4_ending_time
-                        st.assembly_on_days                  = assembly_on_days.__str__()
-                        st.assembly_starting_time            = assembly_starting_time
-                        st.assembly_ending_time              = assembly_ending_time
-                        st.off_days                          = school_off_days.__str__()
+                        st.organization         = user_role.related_organization
+                        st.timing_name          = timing_name
+                        st.school_starting_time = school_start_time
+                        st.school_closing_time  = school_closing_time
+                        st.no_of_periods        = int(total_periods)
+                        st.assembly_on_days     = assembly_on_days.__str__()
+                        st.assembly_starting_time = school_start_time
+                        st.assembly_ending_time  = (school_start_time + assembly_duration)
+                        st.off_days             = school_off_days.__str__()
                         st.save()
                     except Exception as e:
                         messages.warning(request, str(e))
