290a74d78342920c120372bfc2b388de29c8ab0a
Aravind R Pillai <aravind.ramachandran.pillai@pwc.com>
2018-12-12 14:23:08 +0530

    ui active class

--- apps/batch/views.py
+++ apps/batch/views.py
@@ -5,5 +5,5 @@ from apps.utilities.helper.ui_data_helper import UIDataHelper
 
 def index(request):
     template = loader.get_template("batch_home.html")
-    data = UIDataHelper(request).getData(page="is_attendance_home")
+    data = UIDataHelper(request).getData(page="is_batch_home")
     return HttpResponse(template.render(data, request))
