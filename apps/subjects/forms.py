0e01082bb61c88797b93b295362660b5caf60fd9
Pranav Narayanan <pranavn21@gmail.com>
2019-01-13 16:50:10 +0530

    Changes to Subject filter and addition

--- apps/subjects/forms.py
+++ apps/subjects/forms.py
@@ -4,6 +4,18 @@ from django import forms
 from apps.subjects.forms_helper import SubjectFormsHelper
 from properties.session_properties import SessionProperties
 
+class FORM_ClassNamesForSubject(forms.Form) :
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(FORM_ClassNamesForSubject, self).__init__(*args, **kwargs)
+        fh = SubjectFormsHelper(self.request)
+        self.fields['class_names'].choices = fh.getClassesForFilter()
+        self.fields['class_div'].choices = fh.getDivisionForFilter()
+
+    class_names = forms.ChoiceField(choices=[], required=False, initial=None)
+    class_div = forms.ChoiceField(choices=[], required=False, initial=None)
+
 
 class FORM_SubjectDetails(forms.Form) :
 
@@ -11,7 +23,7 @@ class FORM_SubjectDetails(forms.Form) :
         self.request = kwargs.pop('request', None)
         super(FORM_SubjectDetails, self).__init__(*args, **kwargs)
         roleData = json.loads(self.request.session[SessionProperties.USER_SELECTED_ROLE_KEY])
-        self.fields['assign_to_class'].choices = SubjectFormsHelper().getClasses(roleData)
+        self.fields['assign_to_class'].choices = SubjectFormsHelper(self.request).getClasses(roleData)
 
 
     subject_name = forms.CharField(
